de28895a9afb90845542b9a2932674d9f86da4e3
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-11-24 16:41:17 -0700

    WORKING alternating_case

--- codewars/typescript/8-kyu/alTeRNatING_cASe.ts
+++ codewars/typescript/8-kyu/alTeRNatING_cASe.ts
@@ -1,18 +1,30 @@
-let soft = 'HeLLo WoRLD'
-
 function toAlternatingCase(soft) {
   const softAsArray = soft.split('')
   const UTFArray = softAsArray.map((letter) => letter.charCodeAt())
-}
+  let numberString = ''
+  let numberStringConverter = ''
 
-function toAlternatingCase(soft) {
-  const softAsArray = soft.split('')
-  const UTFArray = softAsArray.map((letter) => letter.charCodeAt())
-  UTFArray.map((number) => {
-    if (number <= 90 && number >= 65)
-      return String.fromCharCode(number).toLowerCase()
-    else if (number <= 122 && number >= 97)
-      return String.fromCharCode(number).toUpperCase()
-    else return number
+  let transformedArray = UTFArray.map((number) => {
+    if (number <= 90 && number >= 65) {
+      numberString = String.fromCharCode(number)
+      numberStringConverter = numberString.toLowerCase()
+      return numberStringConverter
+    } else if (number <= 122 && number >= 97) {
+      numberString = String.fromCharCode(number)
+      numberStringConverter = numberString.toUpperCase()
+      return numberStringConverter
+    } else return String.fromCharCode(number)
   })
+  return transformedArray
 }
+
+let soft = 'HellO WorlD'
+
+toAlternatingCase(soft)
+
+// let soft = 'HeLLo WoRLD'
+
+// function toAlternatingCase(soft) {
+//   const softAsArray = soft.split('')
+//   const UTFArray = softAsArray.map((letter) => letter.charCodeAt())
+// }
