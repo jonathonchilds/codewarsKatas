d8d1ffe4784f5f12a01f59545aae5969586ff099
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-11-24 22:11:49 -0700

    adds other solutions

--- codewars/typescript/8-kyu/alTeRNatING_cASe.ts
+++ codewars/typescript/8-kyu/alTeRNatING_cASe.ts
@@ -17,3 +17,16 @@ function toAlternatingCase(soft) {
 let soft = 'HellO WorlD'
 
 toAlternatingCase(soft)
+
+
+- - - - - ALSO WORKS - - - - - 
+// export function toAlternatingCase(s: string): string {
+//   return s.split("").map(a => a === a.toUpperCase()? a.toLowerCase(): a.toUpperCase()).join('')
+// }
+
+// export function toAlternatingCase(s: string): string {
+//   return s.replace(/./g, x => /[a-z]/.test(x) ? x.toUpperCase() : x.toLowerCase());
+// }
+
+
+
