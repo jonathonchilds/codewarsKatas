ae35d46e966a9964a8f0f348cec95f9d47c9b1a4
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-11-24 21:12:26 -0700

    satisfies codewars!

--- codewars/typescript/8-kyu/alTeRNatING_cASe.ts
+++ codewars/typescript/8-kyu/alTeRNatING_cASe.ts
@@ -1,30 +1,19 @@
 function toAlternatingCase(soft) {
   const softAsArray = soft.split('')
-  const UTFArray = softAsArray.map((letter) => letter.charCodeAt())
+  const UTFArray = softAsArray.map((letter) => letter.charCodeAt()) //<--- needed a 0 as an argument to satisfy typescript!!!
   let numberString = ''
   let numberStringConverter = ''
 
   let transformedArray = UTFArray.map((number) => {
     if (number <= 90 && number >= 65) {
-      numberString = String.fromCharCode(number)
-      numberStringConverter = numberString.toLowerCase()
-      return numberStringConverter
+      return String.fromCharCode(number).toLowerCase()
     } else if (number <= 122 && number >= 97) {
-      numberString = String.fromCharCode(number)
-      numberStringConverter = numberString.toUpperCase()
-      return numberStringConverter
+      return String.fromCharCode(number).toUpperCase()
     } else return String.fromCharCode(number)
   })
-  return transformedArray
+  return transformedArray.join('')
 }
 
 let soft = 'HellO WorlD'
 
 toAlternatingCase(soft)
-
-// let soft = 'HeLLo WoRLD'
-
-// function toAlternatingCase(soft) {
-//   const softAsArray = soft.split('')
-//   const UTFArray = softAsArray.map((letter) => letter.charCodeAt())
-// }
