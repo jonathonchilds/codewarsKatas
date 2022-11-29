1aee13515767c7d8fe73b4dbd5717a5dcb7dce3d
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-11-28 19:12:38 -0700

    xor_operator

---
+++ codewars/typescript/8-kyu/xor_operator.ts
@@ -0,0 +1,15 @@
+export const xor = (a: boolean, b: boolean): boolean => {
+  return a !== b
+}
+
+//   return a === false && b === false ? false :
+//          a === true && b === true ? false :
+//          true;
+//
+
+// In some scripting languages like PHP, there exists a logical operator (e.g. &&, ||, and, or, etc.) called the "Exclusive Or" (hence the name of this Kata). The exclusive or evaluates two booleans. It then returns true if exactly one of the two expressions are true, false otherwise. For example:
+
+// false xor false == false // since both are false
+// true xor false == true // exactly one of the two expressions are true
+// false xor true == true // exactly one of the two expressions are true
+// true xor true == false // Both are true.  "xor" only returns true if EXACTLY one of the two expressions evaluate to true.
